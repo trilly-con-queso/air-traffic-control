d97dca44fdcb6b8ec0fd2f1c0b191ebc5a713cc2
Aaron Brandenberger <a.branden.mi@gmail.com>
2023-12-16 16:40:55 -0700

    Add port

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const { App } = require('@slack/bolt');
 require('dotenv').config();
 
-// Initializes your app with your bot token and signing secret
+// Initialize app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // add this
-  appToken: process.env.SLACK_APP_TOKEN // add this
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
 });
 
 (async () => {
-  // Start your app
+  // Start app
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
